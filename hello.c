4f401a9a07ca8adf186d0c775cf90dff830f9bc7
樊委 <31919544+fanweijs@users.noreply.github.com>
2019-10-15 08:48:34 +0800

    Update hello.c
    bug

--- hello.c
+++ hello.c
@@ -3,15 +3,16 @@
 int main(void)
 {
 CURL *curl;
-CURL code res;
+CURLcode res;
+  
 curl=curl_easy_init();
 if(curl){
   curl_easy_setopt(cur1,CURLOPT_URL,"http://myip.ipip.net/");
   res=curl_easy_perform(curl); 
-  if(res !=CURLE_0K)
+  if(res != CURLE_0K)
     fprintf(stderr, "curl_easy_perform() failed:%s\n",
       curl_easy_strerror(res));
     curl_easy_cleanup(curl);
 }
-reture 0;
+return 0;
 }
