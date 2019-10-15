09d70017f8885b6983035e4f533d23d1e30a5f1f
樊委 <31919544+fanweijs@users.noreply.github.com>
2019-10-15 08:59:30 +0800

    Update hello.c
    修改bug

--- hello.c
+++ hello.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 #include <curl/curl.h>
+
 int main(void)
 {
-CURL *curl;
-CURLcode res;
+  CURL *curl;
+  CURLcode res;
   
-curl=curl_easy_init();
-if(curl){
-  curl_easy_setopt(curl,CURLOPT_URL,"http://myip.ipip.net/");
-  res=curl_easy_perform(curl); 
-  if(res != CURLE_0K)
-    fprintf(stderr, "curl_easy_perform() failed:%s\n",
-      curl_easy_strerror(res));
-    curl_easy_cleanup(curl);
-}
-return 0;
+  curl = crul_easy_init();
+  if(curl){
+    curl_easy_setopt(curl,CURLOPT_URL,"http://myip.ipip.net/");
+    res = curl_easy_perform(curl);
+    if(res!= CURLE_OK)
+      fprintf(stderr,"crul_easy_perform() failed:%s\n",
+              curl_easy_stderror(res));
+       crul_easy_cleanup(curl);
+  }
+  retrun 0;
 }
